e95f3314fe32de14693a41adeacccdd37c4921cc
s4izh <sergiosanz234@gmail.com>
2023-05-25 23:22:21 +0200

    updated game.h

--- project/include/game.h
+++ project/include/game.h
@@ -1,17 +1,28 @@
 #ifndef __GAME_H__
 #define __GAME_H__
 
-#define INIT_SHARED 284
-
 #include <libc.h>
 
 extern float frames;
 
 float get_fps();
-float get_frames();
+
+void draw_hangman(int tries);
+
+void clear_hangman();
 
 void clear_screen();
+
 void welcome_screen();
 
+void marco();
+
+void initial_screen();
+
+void init_reader();
+
+void game_loop();
+
+void init_game();
 
 #endif /* __GAME_H__ */
